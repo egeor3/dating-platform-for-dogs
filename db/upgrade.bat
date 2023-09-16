cf0d1c0dc1108d3c074e2a9ed9374204b88de257
eldho george <eldhoig45@gmail.com>
2023-09-16 15:32:48 +0100

    This is the progress that eldho george has made

--- db/upgrade.bat
+++ db/upgrade.bat
@@ -1,1 +1,1 @@
-liquibase --driver=com.mysql.jdbc.Driver --classpath="c:\Program Files\MySQL\ConnectorJ\mysql-connector-java-5.1.41-bin.jar" --changeLogFile=db-changelog.xml --url="jdbc:mysql://localhost:3306/dating" --username=webuser --password=aSecretPass update
+liquibase --driver=oracle.jdbc.driver.OracleDriver --classpath="D:\OracleDB\driver\ojdbc11.jar" --changeLogFile=db-changelog.xml --url="jdbc:oracle:thin:@//localhost:1521/XEPDB1" --username=john --password=abcd1234 update
